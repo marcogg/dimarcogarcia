22d44f5978523678f2ee3c70a7462deb11c9e654
Marco Garcia <marcogarcia.gon@gmail.com>
2022-10-27 18:12:01 -0500

    Header and 2 sections done in desktop

---
+++ js/projects.js
@@ -0,0 +1,22 @@
+const projects = {
+        'Web development':{
+            'Enel':{
+                projectName: 'Development of Rewards Platform',
+                client: 'Enel Codensa',
+                caption: 'UX refinement and front end development',
+                image: '#',
+                description: ''
+
+            }
+        },
+        'UX/UI':{
+
+        },
+        'Social Media':{
+
+        },
+        'Digital Marketing':{
+
+        }
+        
+}

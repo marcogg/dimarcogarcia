4a26809a9d12cc548d257b7023bc75a3905f2ff9
Marco Garcia <marcogarcia.gon@gmail.com>
2022-12-09 16:31:23 -0600

    Project Categories in different files

--- js/functions.js
+++ js/functions.js
@@ -57,65 +57,22 @@ for (let cat = 0; cat < categories.length; cat++) {
 // PROJECTS BUILDER
 
 const categoryFilter = () => {
-    categories[cat].addEventListener("click", function(e) {
-        categoryFilter = e.id
-        return console.log(categoryFilter)
-    })
-}
-
-
+        categories[cat].addEventListener("click", function(e) {
+            let categoryFilterRes = e.id
+            return console.log(categoryFilterRes)
+        })
+    }
+    // Creating Protoype for card
 
 class ProjectCard {
-    constructor(projectName, image, caption) {
-        this.projectName = projectName
+    constructor(image, projectName, caption, link) {
         this.image = image
+        this.projectName = projectName
         this.caption = caption
+        this.link = link
     }
 
     createCard() {
-        const cardProj = document.createElement('div')
-        cardProj.classList.add('card')
-        cardProj.innerHTML = `
-                    <div class="imageContainer">
-                        <div class="overlay"></div>
-                        <img src=${this.image} class="w-100">
-                    </div>
-                    <div class="meta">
-                        <div class="row w-100">
-                            <div class="col-12">
-                                <h5 class="mt-4">${this.projectName}</h5>
-                                <p class="mb-4">${this.caption}</p>
-                                <span class="mt-2">Read More</span>
-                            </div>
-                        </div>
-                    </div>`
 
-        boxProjects.appendChild(cardProj)
-
-    }
-    createCatCards = (proj) => {
-        for (data in proj) {
-            let cards = data.createCard(this.projectName, this.image, this.caption)
-            return cards
-        }
     }
 }
-
-
-
-// const cardPerProject = (db) => {
-//         for (projectData in db) {
-//             let cardCreated = new ProjectCard(this.projectName, this.image, this.caption)
-//             return cardCreated
-//         }
-//     }
-// const webDev = new ProjectCard('Enel Conecta - rewards', './../img/projects/enel_rewards.png', 'UX refinement and front end development')
-
-// const webDev = (projects) => {
-//     Object.values(projects).map(entry => {
-//         return webDev = new ProjectCard(this.projectName, this.image, this.caption)
-//     })
-// }
-const webDev = new ProjectCard('Enel Conecta - rewards', './../img/projects/enel_rewards.png', 'UX refinement and front end development')
-
-document.addEventListener('DOMContentLoaded', projects.createCatCards(projects))

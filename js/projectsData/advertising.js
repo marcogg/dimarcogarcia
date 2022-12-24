98af5381252f7aa14483e2448eb0884b190fb56a
Marco Garcia <marcogarcia.gon@gmail.com>
2022-12-24 02:16:51 -0600

    Advertising projects done

--- js/projectsData/advertising.js
+++ js/projectsData/advertising.js
@@ -2,20 +2,20 @@ const advertising = [{
         projectName: 'Reto mazda 2',
         client: 'Mazda',
         year: 2014,
-        caption: 'Live and digital marketing project',
-        image: '../img/projects/enel_rewards.png',
+        caption: 'BTL + Digital experience to increase engagement among university students',
+        image: '../img/projects/retoMazda2.jpg',
         description: 'TBD',
         url: 'https://twitter.com/hashtag/RetoMazda2?src=hashtag_click',
         skills: ['Graphic Design', 'Industrial design', 'UX', 'UI', 'HTML', 'CSS', 'JS', 'Responsive Design', 'Team Work', 'Front End Dev', 'SEO']
 
     },
     {
-        projectName: 'Viaja en auto comoda y segura',
+        projectName: 'Pijamada Saba con Yuya',
         client: 'Saba',
         year: 2015,
-        caption: '',
-        image: '../img/projects/enel_rewards.png',
-        description: 'Saba is one of the most representative brands in LatAm regarding feminine hygiene, inspired by the successful dynamic of Mada, we replicated the phygital campaign, where the participants needed to accomplish a series of challenges mostly in social media. i was in charge of developing the visual assets, score online platform and social media posts for the brand, while keeping an eye in marketing KPIs.',
+        caption: 'Fulfill the challenges to earn a meet and greet experience with influencer Yuya',
+        image: '../img/projects/pijamadaSaba.jpg',
+        description: 'Saba is one of the most representative brands in LatAm regarding feminine hygiene, inspired by the successful dynamic of Mazda, we replicated the phygital campaign, where the participants needed to accomplish a series of challenges mostly in social media. I was in charge of developing the visual assets, score online platform and social media posts for the brand, while keeping an eye in marketing KPIs.',
         url: 'https://www.facebook.com/Saba.mx/videos/972337419495987/',
         skills: ['Graphic Design', 'Industrial deign', 'UX', 'UI', 'HTML', 'CSS', 'JS', 'Responsive Design', 'Team Work', 'Front End Dev']
 
@@ -24,21 +24,43 @@ const advertising = [{
         projectName: 'Mazda Social Media',
         client: 'Mazda',
         year: 2015,
-        caption: '',
-        image: '../img/projects/enel_rewards.png',
+        caption: 'Enhance engagement to the brand through inbound marketing in Social Media',
+        image: '../img/projects/mazda_socialMedia.jpg',
         description: 'My team was in charge of content and inbound marketing for Mazda Mexico brand, creating visual assets that achieve interaction among the recent followers in Facebook and Twitter. My job was to create the visuals, following the VIS guidelines of Mazda, while contribuiting with ideas for dynamics and campaigns.',
         url: 'https://www.facebook.com/Saba.mx/videos/972337419495987/',
         skills: ['Graphic Design', 'Team Work', 'Creativity']
 
     },
     {
-        projectName: 'Test rides',
-        client: 'Harley Davidson',
-        year: 2016,
-        caption: '',
-        image: '../img/projects/enel_rewards.png',
-        description: 'My team was in charge of content and inbound marketing for Mazda Mexico brand, creating visual assets that achieve interaction among the recent followers in Facebook and Twitter. My job was to create the visuals, following the VIS guidelines of Mazda, while contribuiting with ideas for dynamics and campaigns.',
-        url: 'https://www.facebook.com/Saba.mx/videos/972337419495987/',
+        projectName: 'Zip México arrival',
+        client: 'Zip',
+        year: 2021,
+        caption: 'Setting up the arrival of the global fintech in Mexico, creating digital and offline marketing campaigns',
+        image: '../img/projects/zipArrival.jpg',
+        description: 'Zip is a global fintech that allows online buyers to divide their purchases in 4 installments interest free, without the need of a credit card. My job was to set up all the digital, graphic and audiovisuals assets for the release of the brand in mexican market.',
+        url: 'https://zip.co/au',
+        skills: ['Graphic Design', 'Team Work', 'Creativity']
+
+    },
+    {
+        projectName: 'Real State Marketing',
+        client: 'Grupo Hodaya',
+        year: 2021,
+        caption: 'Form brand identity, to VR experiences living in a webApp. 3 years creating marketing assets for real state',
+        image: '../img/projects/ghodaya.jpg',
+        description: 'Taking advantage of my 3d design skills and my advertising background forma agencies, I spent more than 3 years in Grupo Hodaya, a real state company, where i developed from brand identity, logos, web design and digital sales funnels for each project. Starting from scratch, an architectural blueprint, we created the visual communication, and brought digital experiences to a traditional real state business.',
+        url: '',
+        skills: ['Graphic Design', 'Team Work', 'Creativity']
+
+    },
+    {
+        projectName: 'More than 100 local businesses',
+        client: 'Various',
+        year: 2021,
+        caption: 'Logo Design, brand building, social media marketing and more!',
+        image: '../img/projects/ghodaya.jpg',
+        description: 'Throughout 10 years I hacve helped more than 100 small stores, restaurants, and merchants to bring their business to web. Creating online stores, creative advertising, while keeping an eye in KPIs, in order to optimize their efforts.',
+        url: '',
         skills: ['Graphic Design', 'Team Work', 'Creativity']
 
     },
